c15022b07d1253cc46a5827098442b324ec483f0
falahputra <putrafalah9@gmail.com>
2021-05-08 01:05:50 +0800

    Tes test

--- app/src/main/java/com/ilhamfidatama/androidwithkotlin/HomeActivity.kt
+++ app/src/main/java/com/ilhamfidatama/androidwithkotlin/HomeActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_home.*
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity()  {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
